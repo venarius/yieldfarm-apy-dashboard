3c1d5435d94c34ee581106151a0ac241f0ced957
venarius <tim.lange@titanhq.de>
2021-02-15 17:07:05 +0000

    NEW: Added refresh timer

--- components/Topbar.vue
+++ components/Topbar.vue
@@ -2,10 +2,48 @@
   <div>
     <div class="px-4 py-2 bg-white border-b border-gray-300 flex items-center justify-between">
       <p class="text-xs text-gray-700 font-light">Made with ❤️ for Binance Hackathon: The Future is now</p>
-      <p class="text-xs text-gray-700 font-light">Next refresh: 30 secs</p>
+      <p class="text-xs text-gray-700 font-light">Next refresh: {{ timer }} sec(s)</p>
     </div>
     <div class="p-4 bg-white border-b border-gray-300">
       <p class="text-xl font-bold">YieldFarm Dashboard</p>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import { fetchAPYs } from '../apy/fetcher'
+
+export default Vue.extend({
+  data () {
+    return {
+      timer: 60,
+      timerInterval: null
+    }
+  },
+  mounted () {
+    this.startTimer()
+  },
+  beforeDestroy () {
+    clearInterval(this.timerInterval)
+  },
+  methods: {
+    startTimer () {
+      this.timerInterval = setInterval(() => {
+        this.timer -= 1
+        if (this.timer === -1) {
+          this.timer = 60
+        }
+        if (this.timer === 2) {
+          this.refreshAPYs()
+        }
+      }, 1000, this)
+    },
+    async refreshAPYs () {
+      const apys: any[] = await fetchAPYs()
+      this.$store.commit('apy/setAPYs', apys)
+    }
+  }
+})
+</script>
