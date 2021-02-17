5ed92c72bc4e5147c41fc51d743c8a81a07b3295
venarius <tim.lange@titanhq.de>
2021-02-17 00:05:39 +0000

    NEW: Optical changes

--- components/ApyCard.vue
+++ components/ApyCard.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="border bg-white p-4 shadow-lg rounded-lg apy-card" style="border-color: #eaeaea;">
-    <div class="flex items-center justify-between">
+    <div class="flex items-start justify-between">
       <div class="flex items-center">
-        <img :src="`https://bscscan.com/${token1Image}`" style="height:32px;width:32px;">
+        <div class="flex items-center justify-center border-2 border-gray-500 rounded-full" style="width:3.5rem;height:3.5rem;">
+          <img :src="`https://bscscan.com/${token1Image}`" style="height:32px;width:32px;">
+        </div>
         <repeat-icon class="mx-2" />
-        <img :src="`https://bscscan.com/${token2Image}`" style="height:32px;width:32px;">
+        <div class="flex items-center justify-center border-2 border-gray-500 rounded-full" style="width:3.5rem;height:3.5rem;">
+          <img :src="`https://bscscan.com/${token2Image}`" style="height:32px;width:32px;">
+        </div>
       </div>
       <p class="text-lg ml-1">{{ apy.lpSymbol }}</p>
     </div>
-    {{ apy.apy }}%
+    <p class="font-semibold text-xl" style="color:#7FFF00">APY: {{ apy.apy }}%</p>
     Total in quote: {{ parseInt(apy.lpTotalInQuoteToken).toFixed(2) }}
   </div>
 </template>
